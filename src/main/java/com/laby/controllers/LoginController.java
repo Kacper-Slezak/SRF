22caf249e68c621604809e6f7bc3b039be888bb0
nix746 <twozniak@student.agh.edu.pl>
2025-01-11 13:40:21 +0100

    pierwszy layout loginu

--- src/main/java/com/laby/controllers/LoginController.java
+++ src/main/java/com/laby/controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.laby.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public TextField usernameTextField;
+    public PasswordField passwordField;
+    public Button logInButton;
+    public Button registerButton;
 }

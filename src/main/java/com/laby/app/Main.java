070cb2cda4ae9cea31975642c3f1f7377328a2a4
nix746 <twozniak@student.agh.edu.pl>
2025-01-11 14:55:33 +0100

    proba wlaczenia programu

--- src/main/java/com/laby/app/Main.java
+++ src/main/java/com/laby/app/Main.java
@@ -1,17 +1,28 @@
 package com.laby.app;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main extends Application {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        //TODO naprawic ladowanie logowania
+        //mam identycznie jak w samochodzie i tam tak dziala, co jest nie tak??
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/login.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+        stage.setTitle("SRF");
+        stage.setScene(scene);
+        stage.show();
     }
 }

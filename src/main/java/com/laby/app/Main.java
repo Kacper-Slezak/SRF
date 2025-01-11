f687c93d7b41353c9d5d40a23baea771a6846f69
nix746 <twozniak@student.agh.edu.pl>
2025-01-11 23:49:54 +0100

    goal

--- src/main/java/com/laby/app/Main.java
+++ src/main/java/com/laby/app/Main.java
@@ -31,5 +31,7 @@ public class Main extends Application {
         stage.setTitle("SRF");
         stage.setScene(scene);
         stage.show();
+        //TODO scene manager??
+        //https://www.youtube.com/watch?v=RifjriAxbw8
     }
 }

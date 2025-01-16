1d2e3d87b49cfd297129fc6b186b7357522c6c42
nix746 <twozniak@student.agh.edu.pl>
2025-01-16 23:58:57 +0100

    allroud IMPORTANT home clear

--- src/main/java/com/srf/utils/AlertManager.java
+++ src/main/java/com/srf/utils/AlertManager.java
@@ -16,6 +16,23 @@ public class AlertManager {
         alert.setContentText(content);
         alert.showAndWait();
     }
+
+    public void showError(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public void showInfo(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
     public static AlertManager getInstance() {
         if (instance == null) {
             instance = new AlertManager();

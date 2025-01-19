ea6ace83a0960ddb6fe7ee6d006bde1da54db3cf
Kacper <k.slezak2207@gmail.com>
2025-01-19 16:12:46 +0100

    Older movies link problem solved

--- src/main/java/com/srf/services/imdbService.java
+++ src/main/java/com/srf/services/imdbService.java
@@ -12,18 +12,21 @@ public class imdbService {
     }
 
     public static String fetchImdbUrl(int movieId) {
-        String imdbBaseUrl = "https://www.imdb.com/title/tt0";
+        String imdbBaseUrl = "https://www.imdb.com/title/tt";
         try {
             String imdbId = imdbDAO.getImdbIdByMovieId(movieId);
-            if (imdbId != null && imdbId.length() == 7) {
+            if (imdbId != null) {
+                // Upewnij się, że IMDb ID ma dokładnie 7 znaków, dodając wiodące zera w razie potrzeby
+                imdbId = String.format("%07d", Integer.parseInt(imdbId));
                 return imdbBaseUrl + imdbId;
-            } else if (imdbId != null && imdbId.length() == 6) {
-                return imdbBaseUrl + '0' + imdbId;
             }
         } catch (SQLException e) {
             System.err.println("Failed to fetch IMDb ID: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid IMDb ID format: " + e.getMessage());
         }
         return null;
     }
+
 }
 

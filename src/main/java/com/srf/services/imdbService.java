11e057ae7248c023486b524d15b7e8cf91def597
Kacper <k.slezak2207@gmail.com>
2025-01-19 16:08:13 +0100

    Older movies link problem solved

--- src/main/java/com/srf/services/imdbService.java
+++ src/main/java/com/srf/services/imdbService.java
@@ -15,8 +15,10 @@ public class imdbService {
         String imdbBaseUrl = "https://www.imdb.com/title/tt0";
         try {
             String imdbId = imdbDAO.getImdbIdByMovieId(movieId);
-            if (imdbId != null && !imdbId.isEmpty()) {
+            if (imdbId != null && imdbId.length() == 7) {
                 return imdbBaseUrl + imdbId;
+            } else if (imdbId != null && imdbId.length() == 6) {
+                return imdbBaseUrl + '0' + imdbId;
             }
         } catch (SQLException e) {
             System.err.println("Failed to fetch IMDb ID: " + e.getMessage());
